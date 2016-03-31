934b58c6175f324c471bc8adfafebb16c64c64ec
heleneshaikh <helene.shaikh@gmail.com>
2016-03-31 17:36:13 +0200

    add model and controller test

--- testing-model-controller.js
+++ testing-model-controller.js
@@ -59,6 +59,17 @@ var model = {
 		return true;
 	}
 };
+/* TEST */
+model.fire("53");
+model.fire("06");
+model.fire("16");
+model.fire("26");
+model.fire("34");
+model.fire("24");
+model.fire("44");
+model.fire("12");
+model.fire("11");
+model.fire("10");
 
 var controller = {
 
@@ -83,3 +94,10 @@ function parseGuess(guess) { //B1
 	}
 	return null;
 }
+
+/* TEST */
+console.log(parseGuess("A0"));
+console.log(parseGuess("B6"));
+console.log(parseGuess("G3"));
+console.log(parseGuess("H0"));
+console.log(parseGuess("A7"));
